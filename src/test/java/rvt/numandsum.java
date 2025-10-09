87fe4ec0f78f3287021c46e081912788aacd1bfd
Krists Krupņiks <a240333kk@rvt.lv>
2025-10-09 14:41:58 +0300

    skaitlu daudzums un skaits tests

---
+++ src/test/java/rvt/numandsum.java
@@ -0,0 +1,10 @@
+package rvt;
+
+public class numandsum {
+    public static void main(String[] args) {
+    
+    for (int i = 100; i > 10; i--) {
+        System.out.println(i);
+        }
+    }
+}
