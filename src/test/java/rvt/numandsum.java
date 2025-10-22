1881b4988d0fd3271f529c2375f598e126d567a5
Krists Krupņiks <a240333kk@rvt.lv>
2025-10-22 14:17:37 +0300

    Uzdevums

--- src/test/java/rvt/numandsum.java
+++ src/test/java/rvt/numandsum.java
@@ -1,10 +1,24 @@
 package rvt;
 
+import java.util.Scanner;
+
+
 public class numandsum {
     public static void main(String[] args) {
-    
-    for (int i = 100; i > 10; i--) {
-        System.out.println(i);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Pirmais skaitlis? ");
+        int pirmais = Integer.parseInt(scan.nextLine().trim());
+
+        System.out.print("Pedejais skaitlis? ");
+        int pedejais = Integer.parseInt(scan.nextLine().trim());
+
+        int sum = 0;
+        for (int i = pirmais; i <= pedejais; i++) {
+            sum += i;
         }
+
+        System.out.println("The sum is " + sum);
+        scan.close();
     }
 }
