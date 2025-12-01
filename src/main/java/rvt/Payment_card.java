9cdaf5cc49640db35eb08eecd53b08ecbbca0803
Krists Krupņiks <a240333kk@rvt.lv>
2025-12-01 09:44:38 +0200

    Payment Card

---
+++ src/main/java/rvt/Payment_card.java
@@ -0,0 +1,68 @@
+package rvt;
+import java.util.Scanner;
+public class Payment_card {
+
+    public static void main(String[] args) {
+
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+
+        mattsCard.addMoney(50);
+
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+    }
+}
+
+
+class PaymentCard {
+
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        if (amount < 0) {
+            return;
+        }
+
+        this.balance += amount;
+        if (this.balance > 150) {
+            this.balance = 150;
+        }
+    }
+}
+
